2a67460e06849093585615b9e292c225e6ec5527
tuantq29 <tuantq29@fpt.com>
2025-08-03 10:45:00 +0700

    change database connection config

--- data/database.js
+++ data/database.js
@@ -6,6 +6,8 @@ const dbPassword = process.env.MONGODB_PASSWORD;
 const dbName = process.env.MONGODB_DB_NAME;
 
 const uri = `mongodb+srv://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
+
+console.log('uri', uri)
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
@@ -15,7 +17,7 @@ try {
   await client.db(dbName).command({ ping: 1 });
   console.log('Connected successfully to server');
 } catch (error) {
-  console.log('Connection failed.');
+  console.log('Connection failed.', error);
   await client.close();
   console.log('Connection closed.');
 }
